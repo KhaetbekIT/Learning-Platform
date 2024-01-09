e3a842bb52ea4f4a92fdef9754a4b774ae978fbb
AzizjonIT <125983595+AzizjonIT@users.noreply.github.com>
2024-01-09 12:08:36 +0500

    #fix #js ComboButton componentiga ozgartirish kirtim https://khaetbekit.monday.com/boards/1836096355/pulses/1836420243

--- src/.storybook/ComboButton/index.jsx
+++ src/.storybook/ComboButton/index.jsx
@@ -1,14 +1,29 @@
+import propTypes from "prop-types";
+
 const ComboButton = ({
   children,
   className,
   disabled,
-  label, onClick, size,
-  tooltipAlignment,  translateWithId, kind, shortcut, renderIcon,
+  label,
+  onClick,
+  size,
+  tooltipAlignment,
+  translateWithId,
+  kind,
+  shortcut,
+  renderIcon,
 }) => {
-  const classes = classnames([className, `media__${size}`, "combobutton"]);
+  const classes = classnames([className, `media__${size}`, "ComboButton"]);
   return (
     <div className={classes} disabled={disabled}>
-      {children}
+      <div className="ComboButton__wrap">
+        <p className="ComboButton__label">{label}</p>
+        <button type="button" onClick={onClick}>
+          {" "}
+          {renderIcon}{" "}
+        </button>
+      </div>
+      <div className="ComboButton__content">{children}</div>
     </div>
   );
 };
@@ -21,17 +36,26 @@ ComboButton.propTypes = {
   onClick: propTypes.func,
   size: propTypes.oneOf(["sm", "md", "lg"]),
   tooltipAlignment: propTypes.oneOf([
-    "top", "top-left", "top-right", "bottom",  "bottom-left", "bottom-right", "left", "right",
+    "top",
+    "top-left",
+    "top-right",
+    "bottom",
+    "bottom-left",
+    "bottom-right",
+    "left",
+    "right",
   ]),
   translateWithId: propTypes.func,
   kind: propTypes.oneOfType([propTypes.oneOf(["default", "danger"])]),
   shortcut: propTypes.string,
-  renderIcon: propTypes.oneOf(["func", "object", "basic"]),
+  renderIcon: propTypes.oneOf(["func", "object", "basic"]).isRequired ,
 };
 
 ComboButton.defaultProps = {
   size: "lg",
   kind: "default",
+  label: "Primary action",
+  renderIcon: "basic",
 };
 
 export default ComboButton;
