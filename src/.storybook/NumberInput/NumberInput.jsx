6b4ca1f490da0c06de32e9b2860790853d7d535e
AzizjonIT <125983595+AzizjonIT@users.noreply.github.com>
2024-01-11 10:19:34 +0500

    #fix #js NumberInput componentini yaratim https://khaetbekit.monday.com/boards/1836096355/pulses/1836993872

---
+++ src/.storybook/NumberInput/NumberInput.jsx
@@ -0,0 +1,73 @@
+import { Fragment } from "react";
+import propTypes from "prop-types";
+
+const NumberInput = ({
+  allowEmpty,
+  className,
+  defaultValue,
+  disabled,
+  disableWheel,
+  helperText,
+  hideLabel,
+  hideSteppers,
+  iconDescription,
+  id,
+  invalid,
+  invalidText,
+  label,
+  light,
+  max,
+  min,
+  onChange,
+  onClick,
+  onKeyUp,
+  readOnly,
+  size,
+  slug,
+  step,
+  translateWithId,
+  value,
+  warn,
+  warnText,
+  ...props
+}) => {
+  return (
+    <Fragment>
+      <div className={className} {...props}>
+        <label className="NumberInput_wrap">{children}</label>
+      </div>
+    </Fragment>
+  );
+};
+
+NumberInput.propTypes = {
+  allowEmpty: propTypes.bool,
+  className: propTypes.string,
+  defaultValue: propTypes.oneOf("number", "string"),
+  disabled: propTypes.bool,
+  disableWheel: propTypes.bool,
+  helperText: propTypes.node,
+  hideLabel: propTypes.bool,
+  hideSteppers: propTypes.bool,
+  iconDescription: propTypes.string,
+  id: propTypes.string,
+  invalid: propTypes.bool,
+  invalidText: propTypes.node,
+  label: propTypes.node,
+  light: propTypes.bool,
+  max: propTypes.number,
+  min: propTypes.number,
+  onChange: propTypes.func,
+  onClick: propTypes.func,
+  onKeyUp: propTypes.func,
+  readOnly: propTypes.bool,
+  size: propTypes.oneOf(["sm", "md", "lg"]),
+  slug: propTypes.node,
+  step: propTypes.number,
+  translateWithId: propTypes.func,
+  value: propTypes.oneOf(["number", "string"]),
+  warn: propTypes.bool,
+  warnText: propTypes.node,
+};
+
+export default NumberInput;
